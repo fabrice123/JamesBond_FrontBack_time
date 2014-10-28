74bbc411d0ae4d189e25408b3ca68ef5776936d9
PeterVdK <petervandenkerckhove@Peter-VandenKerckhoves-MacBook-Pro.local>
2014-10-28 10:47:02 +0100

    Bondafbeeldingen staan op het scherm

--- www/js/app.js
+++ www/js/app.js
@@ -30,11 +30,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       controller: 'AppCtrl'
     })
 
-    .state('app.search', {
-      url: "/search",
+    .state('app.bonds', {
+      url: "/bonds",
       views: {
         'menuContent' :{
-          templateUrl: "templates/search.html"
+          templateUrl: "templates/bonds.html"
         }
       }
     })
