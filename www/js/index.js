c1e57fc5dab45a5dfc31cb20bffcb80107ad9a0e
PeterVdK <petervandenkerckhove@Peter-VandenKerckhoves-MacBook-Pro.local>
2014-10-29 15:42:05 +0100

    Schaduw

--- www/js/index.js
+++ www/js/index.js
@@ -2,9 +2,13 @@
  * Created by petervandenkerckhove on 28/10/14.
  */
 
-(function homepage(){
-    $("dotWie").onmouseover(function(){
-        $wie = document.getElementById("wie");
-        $wie.style.color = "red";
-    });
+(function(){
+    function init(){
+        $("#dotWie").mouseover(function(){
+            var wie = document.getElementById("wie");
+            wie.css("color","red");
+        });
+    }
+
+    init();
 }())
